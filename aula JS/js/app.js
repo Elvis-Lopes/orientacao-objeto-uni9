4e30bd710e2789a3ad7f2336dceb8b0bf04591c4
Elvis Lopes <elvislopes1996@hotmail.com>
2020-04-15 19:32:46 -0300

    add da aula anteriro

---
+++ aula JS/js/app.js
@@ -0,0 +1,7 @@
+function exibirDadosDaTela(){
+    
+    var nome = document.getElementById("txtNome").value;
+    var idade = document.getElementById("txtIdade").value;
+
+    alert("Olá "+ nome + " tem " + idade +" anos de idade");
+}
